7ad7b7bc0bafc65dd1271a3f7a9c31bea864215c
aladdin731 <aladdin731@hotmail.com>
2021-05-10 23:57:02 -0700

    179

--- lc_everyday/src/LC_179.java
+++ lc_everyday/src/LC_179.java
@@ -2,17 +2,21 @@
         if(nums == null || nums.length == 0) return "";
         
         String[] res = new String[nums.length];
+        // num -> string 
         for(int i = 0; i < nums.length; i++) {
             res[i] = String.valueOf(nums[i]);
         }
+        // 
         Arrays.sort(res, (a, b) -> {
             String s1 = a + b;
             String s2 = b + a;
+            // if s2 > s1 return 1 从大到小排
             return s2.compareTo(s1);
         });
             
         if(res[0].equals("0")) return "0";
         
+        // StringBuilder -> String
         StringBuilder sb = new StringBuilder();
         for(String s : res) sb.append(s);
         
---
+++ note.md
@@ -0,0 +1,3 @@
+String
+
+1. String.valueOf(xx) 将其他格式转化为string格式
